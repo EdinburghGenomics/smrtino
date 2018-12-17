b9b27ebcaadf91915df970540a4bae247f9f46d6
Tim Booth <tim.booth@ed.ac.uk>
2018-12-17 10:42:16 +0000

    BLOB plotting is ready to test.

---
+++ count_dust.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import sys
+
+# Read the output of dustmasker_static and report the proportion of
+# non-dust (uppercase) per sequence.
+upper = 0
+lower = 0
+count = 0
+# These aren't contigs but we pretend they are...
+contig = None
+
+# This just prints out the proportion
+def emit():
+    if count:
+        total = upper + lower
+        print('0.000' if total == 0 else "{:.3f}".format(upper / total))
+
+# But for blobtools we want something a little different
+print("## count_dust v0.0")
+print("## Total Reads = 10000") # We assume?!
+print("## Mapped Reads = 10000")
+print("## Unmapped Reads = 0")
+print("# contig_id\tread_cov\tbase_cov")
+def emit():
+    if count:
+        total = upper + lower
+        frac = 0.0 if total == 0 else (upper / total)
+        print("{}\t1\t{:.3f}".format(contig, 10**(5*(frac-0.5))))
+
+for l in sys.stdin:
+    if l.startswith('>'):
+        emit()
+        count += 1
+        contig = l.split('|')[1].strip()
+        upper = lower = 0
+    else:
+        upper += sum( 1 for n in l if n in 'ATCG' )
+        lower += sum( 1 for n in l if n in 'atcg' )
+emit()
