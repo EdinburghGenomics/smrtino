7243c42c1c4f00c734b4bc950be8487824d4edb8
Tim Booth <tim.booth@ed.ac.uk>
2018-12-21 11:21:22 +0000

    Tweaks to the histo plot script.

--- plot_histo.R
+++ plot_histo.R
@@ -5,7 +5,7 @@ p <- arg_parser("Create binned subread histogram")
 
 # Add command line arguments & parse
 p <- add_argument(p, "--bins", help="Read length bin boundaries", default = "1K,2K,3K,5K,10K,50K,100K")
-p <- add_argument(p, "--file", help="Table file of histogram data",default=NA)
+p <- add_argument(p, "--file", help="Table file of histogram data", nargs = 1)
 p <- add_argument(p, "--output", help = "Output PNG file, please specify as 'x.png'", default = 'plot_histo_R.png')
 argv <- parse_args(p)
 
@@ -14,13 +14,15 @@ data<-read.table(argv$file)
 bins<-strsplit(argv$bins, ",")
 bins<-gsub("K","000",bins[[1]])
 bins<-unique(sort(as.numeric(bins)))
-if(tail(bins,n=1) < nrow(data)){bins<-append(bins,nrow(data)-1)} 
+if(tail(bins,n=1) < nrow(data)){bins<-append(bins,nrow(data)-1)}
+
+# Set output to the desired file. On a headless system we need to do this before plotting
+# or we get "Error in grid.newpage() : no active or default device"
+png(argv$output, width=600, height=400)
 
 #Make & save the histogram
-ggplot(data = data, aes(data$V1))+ 
-  geom_histogram(breaks=bins)+
+ggplot(data = data, aes(data$V1))+
+  geom_histogram(breaks=bins, fill="blue", color="black")+
   ylab("Frequency")+xlab("Read Length")+
   theme_light()
-
-ggsave(argv$output)
 
