a5c8a1ad452153c806927e4a265f8a50384571b7
Tim Booth <tim.booth@ed.ac.uk>
2023-03-23 18:42:43 +0000

    Add a simple test run detector script.

---
+++ is_testrun.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -euo pipefail
+shopt -s nullglob
+
+# A script that, given a Sequel run direcotry, says if this is an auto-test
+# pseudo-run. Return values are:
+# 0 - this is a test run
+# 1 - this is not a test run
+# 2 - not sure (and an error will be printed)
+
+if [ -n "${1:-}" ] ; then
+    cd "$1"
+fi
+
+cells="$(printf "%s" ?_???/)"
+
+if [ -z "$cells" ] ; then
+    # Is this even a run??
+    echo "No cells seen in $(pwd)" >&2
+    exit 2
+elif [ "$cells" != "1_A01/" ] ; then
+    # If there is more than one cell it's not a test run
+    exit 1
+fi
+
+# Annoyingly "test -f" returns 0 ?!
+runmeta="$(printf "%s" 1_A01/.*.run.metadata.xml)"
+if [ -z "$runmeta" ] ; then
+    # Did we wait for .transferdon to appear?
+    echo "No .run.metadata.xml in $(pwd)/1_A01" >&2
+    exit 2
+elif [ ! -f "$runmeta" ] ; then
+    echo "$runmeta is not a file"
+    exit 2
+fi
+
+# This seems a simple check. No need to parse the XML. Just grep.
+grep -qF 'InstrumentId="Inst1234"' "$runmeta"
