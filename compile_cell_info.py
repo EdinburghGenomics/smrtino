48feebd6ad5a04aecd28143764b27de556709d33
Tim Booth <tim.booth@ed.ac.uk>
2024-02-01 12:20:06 +0000

    Updated compile_cell_info. Needs a test.

--- compile_cell_info.py
+++ compile_cell_info.py
@@ -17,21 +17,38 @@ def main(args):
 
     L.basicConfig(level=(L.DEBUG if args.debug else L.WARNING), stream=sys.stderr)
 
+    # Build the links to the files.
     info = gen_info(args)
 
     if args.extract_ids:
-        info.update(extract_ids(args.bcfiles[0]))
+        info.update(extract_ids_multi(args.bcfiles))
 
     dump_yaml(info, fh=sys.stdout)
+
+def extract_ids_multi(yaml_files):
+    """Run extract_ids() on each of the YAML files and verify that
+       the result is the same in all cases, and return the resulting dict.
+    """
+    extracted_ids = extract_ids(yaml_files[0])
+
+    for yf in yaml_files[1:]:
+        alt_ids = extract_ids(yf)
+
+        if alt_ids != extracted_ids:
+            raise RuntimeError(f"extract_ids({yaml_files[0]}) mismatch with extract_ids({yf}):\n"
+                               f"{extracted_ids}\nvs\n{alt_ids}")
+
+    return extracted_ids
+
 
 def extract_ids(yaml_file):
     """Pull some bits needed for link_to_smrtlink.py
     """
-
     ydata = load_yaml(yaml_file)
 
     return { k: ydata[k]
-             for k in "run_id run_slot cell_id cell_uuid".split() }
+             for k in '''run_id run_slot cell_id cell_uuid
+                         ws_name ws_desc'''.split() }
 
 def gen_info(args):
 
@@ -39,17 +56,21 @@ def gen_info(args):
     for k in ['sc_data', 'unassigned']:
         v = getattr(args, k)
         if v and v != "-":
-            res[k] = check_exists(v, args.check_exists)
+            res[k] = check_exists(v, args.check_yaml)
 
-    res['barcodes'] = [ check_exists(f, args.check_exists) for f in args.bcfiles ]
+    # If args.extract_ids is set then there's no point in loading the YAML twice.
+    check_bcfile_yaml = args.check_yaml and (not args.extract_ids)
+    res['barcodes'] = [ check_exists(f, check_bcfile_yaml) for f in args.bcfiles ]
 
     return res
 
-def check_exists(f, really_check=True):
+def check_exists(f, syntax_check=True):
     """Check that a file exists. In fact, check it's valid YAML
     """
-    if really_check:
+    if syntax_check:
         load_yaml(f)
+    else:
+        assert os.path.exists(f)
 
     return '@' + f
 
@@ -66,10 +87,10 @@ def parse_args(*args):
                             help="Location of info.yaml for unassigned reads")
     argparser.add_argument("-d", "--debug", action="store_true",
                             help="Print more verbose debugging messages.")
-    argparser.add_argument("-c", "--check_exists", action="store_true",
-                            help="Check that the files exist.")
+    argparser.add_argument("-c", "--check_yaml", action="store_true",
+                            help="Check that the YAML files can be loaded.")
     argparser.add_argument("-x", "--extract_ids", action="store_true",
-                            help="Add the run_id, cell_id, cell_uuid from the first info file")
+                            help="Add the run_id, cell_id, cell_uuid, ws_name from the info files")
 
     return argparser.parse_args(*args)
 
