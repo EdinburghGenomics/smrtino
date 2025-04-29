77d60381d34a2bacff8f29ce9be31f8da78b342c
Tim Booth <tim.booth@ed.ac.uk>
2025-04-29 14:34:17 +0100

    Fix up the toolbox to work on the new e1000 drive

--- toolbox/align_to_silva.sh
+++ toolbox/align_to_silva.sh
@@ -2,11 +2,12 @@
 
 # This shell script needs to take a single argument (a FASTQ file) and
 # align the reads within to the SILVA database, writing SAM to STDOUT
+BASE="/mnt/lustre/e1000/home/edg01/edg01/shared"
 
 # It should respect the SNAKEJOB_THREADS setting.
-/lustre-gseg/software/minimap2/minimap2-2.26_x64-linux/minimap2 \
+"$BASE"/software/minimap2/minimap2-2.26_x64-linux/minimap2 \
     -a -t ${SNAKEJOB_THREADS:-3} -x asm10 \
-    /lustre-gseg/references/silva/silva138/SILVA_138.1_LSU_SSU_NR99.fasta \
+    "$BASE"/references/silva/silva138/SILVA_138.1_LSU_SSU_NR99.fasta \
     "$1"
 
 # Note - I came up with the above database and threads combo through guesswork!
